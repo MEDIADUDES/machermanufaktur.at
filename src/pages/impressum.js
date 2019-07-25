47a7b621ac39ba03908215dc076938278e55eda6
Simon Mayerhofer <git@maysi.de>
2019-07-25 22:17:42 +0200

    Add impressum text

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -11,6 +11,49 @@ const SiteNotice = () => {
 			</Helmet>
 
 			<h1>Impressum</h1>
+			<p>
+				Offenlegung gemäß § 25 Mediengesetz und § 5 E-Commerce Gesetz:
+				<br />
+				Heldenmacher
+				<br />
+				Dominic Pfeffer
+				<br />
+				Anschrift:
+				<br />
+				Porzelangasse 3, 1090 Wien
+				<br />
+				Tel: <a href="tel:+4369919831028">+43 699 198 310 28</a>
+				<br />
+				E-Mail:{' '}
+				<a
+					href="mailto:office@machermanufaktur.at
+"
+				>
+					office@machermanufaktur.at
+				</a>
+				<br />
+				Web: www.machermanufaktur.at
+			</p>
+			<p>
+				Unternehmensgegenstand:
+				<br />
+				Werbeagentur
+			</p>
+			<p>
+				Rechtsform:
+				<br />
+				Einzelunternehmen
+			</p>
+			<p>
+				UID-Nummer:
+				<br />
+				ATU63599207
+			</p>
+			<p>
+				Kammerzugehörigkeit:
+				<br />
+				Mitglied der WKÖ, WKOÖ, Fachverband Werbung und Marktkommunikation
+			</p>
 		</Page>
 	);
 };
